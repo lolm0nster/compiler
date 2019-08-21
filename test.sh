3bc7c3e296a54e0811d57210208f879e988cf8e6
Kensei Nagato <a11cad2br@gmail.com>
2019-08-21 13:55:55 +0900

    整数一つをコンパイルするコンパイラ

---
+++ test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+try(){
+	excepted="$1"
+	input="$2"
+
+	./9cc "$input" > tmp.s
+	gcc -o tmp tmp.s
+	./tmp
+	actual="$?"
+
+	if [ "$actual" = "$excepted" ]; then
+		echo "$input => $actual"
+	else
+		echo "$input => $excepted excepted, but got $actual"
+		exit 1
+	fi
+}
+
+try 0 0
+try 42 42
+
+echo OK
